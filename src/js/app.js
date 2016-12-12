f2e5f04a3e2a43df245d87ea6d21cedcebea4e4a
JuanMa Ruiz <ruizjuanma@gmail.com>
2016-12-12 01:35:13 +0100

    Added collision function but not working properly

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var canvas = document.getElementById('myCanvas');
-  canvas.width = document.body.clientWidth;
+  canvas.width = document.body.clientWidth * 0.7;
   canvas.height = document.body.clientHeight;
   var ctx = canvas.getContext('2d');
 
@@ -19,6 +19,16 @@
       width: 75,
       xPos: (canvas.width - 75) / 2,
       yPosition: canvas.height - 10
+    },
+    bricks: {
+      color: '#D24D57',
+      rowCount: 3,
+      columnCount: 5,
+      width: 75,
+      height: 20,
+      padding: 10,
+      offsetTop: 30,
+      offsetLeft: 30
     }
   }
 
@@ -26,6 +36,15 @@
   var moveY = -2;
   var rightPressed = false;
   var leftPressed = false;
+  var beginGame = false;
+  var bricks = [];
+
+  for (let c = 0; c < settings.bricks.columnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < settings.bricks.rowCount; r++) {
+      bricks[c][r] = {x: 0, y:0, status: 1};
+    }
+  }
 
   document.addEventListener("keydown", keyDownHandler);
   document.addEventListener("keyup", keyUpHandler);
@@ -47,6 +66,38 @@
     ctx.closePath();
   }
 
+  function drawBricks() {
+    for(let i = 0; i < settings.bricks.columnCount; i++) {
+      for(let j = 0; j < settings.bricks.rowCount; j++) {
+        if( bricks[i][j].status === 1 ) {
+          var brickX = (i * (settings.bricks.width + settings.bricks.padding)) + settings.bricks.offsetLeft;
+          var brickY = (j * (settings.bricks.height + settings.bricks.padding)) + settings.bricks.offsetTop;
+          bricks[i][j].x = brickX;
+          bricks[i][j].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, settings.bricks.width, settings.bricks.height);
+          ctx.fillStyle = settings.bricks.color;
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+
+  function collisionDetection() {
+    for(let i = 0; i < settings.bricks.columnCount; i++) {
+      for(let j = 0; j < settings.bricks.rowCount; j++) {
+        var brick = bricks[i][j];
+        if( brick.status === 1 ) {
+          console.log('brick.x: ', brick.x)
+          if( settings.ball.xPos > brick.x && settings.ball.xPos < brick.x + settings.bricks.width && settings.ball.yPos > bricks.y && settings.ball.yPos < brick.y + settings.bricks.height ) {
+            moveY = -moveY;
+          }
+        }
+      }
+    }
+  }
+
   function draw() {
     /**
      * void ctx.clearRect(x, y, width, height);
@@ -54,31 +105,41 @@
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
+    collisionDetection();
 
-    if( settings.ball.yPos + moveY < settings.ball.radius) {
-      moveY = -moveY;
-    } else if ( settings.ball.yPos + moveY > canvas.height - settings.ball.radius ) {
-      if( settings.ball.xPos > settings.paddle.xPos && settings.ball.xPos < settings.paddle.xPos + settings.paddle.width ) {
+    if( beginGame ) {
+      if( settings.ball.yPos + moveY < settings.ball.radius) {
         moveY = -moveY;
-      } else {
-        alert("Game over!!");
-        document.location.reload();
+      } else if ( settings.ball.yPos + moveY > canvas.height - settings.ball.radius ) {
+        if( settings.ball.xPos > settings.paddle.xPos && settings.ball.xPos + settings.ball.radius < settings.paddle.xPos + settings.paddle.width ) {
+          moveY = -moveY;
+        } else {
+          console.log('settings.ball.xPos: ', settings.ball.xPos);
+          console.log('settings.paddle.xPos: ', settings.paddle.xPos);
+          console.log('settings.ball.xPos: ', settings.ball.xPos);
+          console.log('settings.ball.radius: ', settings.ball.radius);
+          console.log('settings.paddle.xPos: ', settings.paddle.xPos);
+          console.log('settings.paddle.width: ', settings.paddle.width);
+          alert("Game over!!");
+          document.location.reload();
+        }
       }
-    }
 
-    if( settings.ball.xPos + moveX < settings.ball.radius || settings.ball.xPos + moveX > canvas.width - settings.ball.radius) {
-      moveX = -moveX;
-    }
+      if( settings.ball.xPos + moveX < settings.ball.radius || settings.ball.xPos + moveX > canvas.width - settings.ball.radius) {
+        moveX = -moveX;
+      }
 
-    if(rightPressed && settings.paddle.xPos < canvas.width - settings.paddle.width) {
-      settings.paddle.xPos += 7;
-    }
-    if(leftPressed && settings.paddle.xPos > 0) {
-      settings.paddle.xPos -= 7;
-    }
+      if(rightPressed && settings.paddle.xPos < canvas.width - settings.paddle.width) {
+        settings.paddle.xPos += 7;
+      }
+      if(leftPressed && settings.paddle.xPos > 0) {
+        settings.paddle.xPos -= 7;
+      }
 
-    settings.ball.xPos += moveX;
-    settings.ball.yPos += moveY;
+      settings.ball.xPos += moveX;
+      settings.ball.yPos += moveY;
+    }
   }
 
   function keyDownHandler(event) {
@@ -101,6 +162,18 @@
     }
   }
 
-  setInterval(draw, 10);
 
+  var userResponse = window.prompt('Do you want to begin the game?');
+
+  if( userResponse.toLowerCase() === 'yes' ) {
+    beginGame = true;
+  } else if ( userResponse.toLowerCase() === 'no' ) {
+    alert("Ok, refresh the page to begin the game")
+  } else {
+    alert('Please, answer "yes or"')
+  }
+  
+  if( beginGame ) {
+    setInterval(draw, 10);
+  }
 })();
